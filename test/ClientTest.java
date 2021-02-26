578781524c7573c35f2509647742367a28500e99
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2021-02-26 15:51:26 +0000

    ClientTest file created

--- test/ClientTest.java
+++ test/ClientTest.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -40,54 +40,66 @@ public class ClientTest {
     /**
      * Test of main method, of class Client.
      */
-    @Test
-    public void testMain() throws Exception {
-        System.out.println("main");
-        String[] args = null;
-        Client.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testMain() throws Exception {
+//        System.out.println("main");
+//        String[] args = null;
+//        Client.main(args);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of set method, of class Client.
      */
-    @Test
-    public void testSet() {
-        System.out.println("set");
-        String user = "";
-        String host = "";
-        int port = 0;
-        Client instance = new Client();
-        instance.set(user, host, port);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testSet() {
+//        System.out.println("set");
+//        String user = "";
+//        String host = "";
+//        int port = 0;
+//        Client instance = new Client();
+//        instance.set(user, host, port);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of run method, of class Client.
      */
-    @Test
-    public void testRun() throws Exception {
-        System.out.println("run");
-        Client instance = new Client();
-        instance.run();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testRun() throws Exception {
+//        System.out.println("run");
+//        Client instance = new Client();
+//        instance.run();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of loop method, of class Client.
      */
+//    @Test
+//    public void testLoop() throws Exception {
+//        System.out.println("loop");
+//        CLFormatter helper = null;
+//        BufferedReader reader = null;
+//        Client instance = new Client();
+//        instance.loop(helper, reader);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+    /**
+     * Test of main method with exit, of class Client.
+     */
     @Test
-    public void testLoop() throws Exception {
-        System.out.println("loop");
-        CLFormatter helper = null;
-        BufferedReader reader = null;
-        Client instance = new Client();
-        instance.loop(helper, reader);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testMainExit() throws Exception {
+        String[] args = {"username", "localhost", "8888"};
+        String input = "exit\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF8"));
+        System.setIn(in); // Sets System.in to the supplied stream
+        Client.main(args);
     }
     
 }
