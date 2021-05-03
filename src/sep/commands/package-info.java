d08a395769e587cc307c4acb989200f1669f6e6e
Manuel Gomes Rosmaninho <mgrosmaninho@hotmail.com>
2021-05-03 21:36:25 +0100

    javadoc updated

---
+++ src/sep/commands/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A Command Interface and all concrete commands available.
+ */
+package sep.commands;
---
+++ src/sep/tinee/client/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the Main class, a Client UI class and Client State Enum class.
+ */
+package sep.tinee.client;
